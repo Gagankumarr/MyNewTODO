d6b35cc983af7c0031380dca968315afcaa3c630
unknown <gkpippal@gmail.com>
2021-03-28 12:55:06 +0530

    TODO list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { PhotosService } from 'src/services/photos.service';
+// import { PhotosService } from 'src/services/photos.service';
+import { TodosService } from 'src/services/todos.service';
 
 @Component({
   selector: 'app-root',
@@ -8,18 +9,28 @@ import { PhotosService } from 'src/services/photos.service';
 })
 export class AppComponent implements OnInit {
   title = 'jobProject';
-  allPhotos= [];
-constructor( private _photosService: PhotosService){
+  todoList: any;
+  // allPhotos= [];
+  
+constructor( private _todos: TodosService){
 
 }
   ngOnInit(): void {
-    this.getPhotos();
+    this.gettodos();
   }
 
-    getPhotos(){
-      this._photosService.getPhotos(1).subscribe(res => {
+    // getPhotos(){
+    //   this._photosService.getPhotos(1).subscribe(res => {
+    //     console.log(res);
+    //     this.allPhotos=res
+        
+    //   })
+    // }
+
+    gettodos(){
+      this._todos.gettodos().subscribe( res => {
         console.log(res);
-        this.allPhotos=res
+        this.todoList= res;
         
       })
     }
